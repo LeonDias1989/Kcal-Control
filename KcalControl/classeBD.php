5671e737f8dc9f83ae1c74623bc7323ba6cae557
Leon <leondias1989@gmail.com>
2014-10-23 10:58:36 -0200

    Inserindo classes de consulta e consulta refeição

--- KcalControl/classeBD.php
+++ KcalControl/classeBD.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 	class funcoesBD {
 
 		private $conexao;
@@ -61,5 +61,38 @@
 				VALUES ('$ultimoIdInserido', '$idAlimento', '$quantidade')";
 				$resultado = mysqli_query($this->conexao, $inserir) or die ("Não foi possível inserir a Refeição");
 			}
+
+		function selecionarRefeicao(){
+
+			/*Feito dessa meneira apenas para teste, pode ser reformulado o código com o melhor uso da OO*/
+
+			mysql_connect("localhost", "root", "") or die (mysql_error ());
+
+			// Seleciona o Banco de Dados
+			mysql_select_db("kcal") or die(mysql_error());
+
+			//query SQL
+			$strSQL = "SELECT refeicao.nome, refeicao.data, refeicao_alimento.quantidade, 
+			(select alimentos.nome from alimentos where refeicao_alimento.id_alimento = alimentos.id ) as alimento_nome 
+			FROM `refeicao` inner join refeicao_alimento where refeicao.id = refeicao_alimento.id_refeicao";
+
+			// Executa a query (o recordset $rs contém o resultado da query)
+			$rs = mysql_query($strSQL);
+			
+			// Loop pelo recordset $rs
+			// Cada linha vai para um array ($row) usando mysql_fetch_array
+			while($row = mysql_fetch_array($rs)) {
+
+			   // Escreve o valor da coluna FirstName (que está no array $row)
+			  echo "Nome da Refeição: ".$row['nome'] ." | Data: " .$row['data']  
+			  ." | Quantidade: " .$row['quantidade']  ." | Nome do Alimento: " .$row['alimento_nome']
+			  ."<br />";
+
+			  }
+
+			// Encerra a conexão
+			mysql_close();
+					
+		}
 	}
 ?>
